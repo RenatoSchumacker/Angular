e4a60418250f967954ed6c9cb6599efa43ac6efc
RenatoSchumacker <renatoschumacker45@gmail.com>
2021-11-24 22:55:01 -0300

    24-11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { XyzComponent } from './xyz/xyz.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './views/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
